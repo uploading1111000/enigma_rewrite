f71d1c9a1a0433a0203c12d197e817ec5b40d33d
uploading1111000 <alexkdaniels@icloud.com>
2023-02-27 21:26:25 +0000

    ngram no work with sets

--- src/Solver/Cpp/analysis/NGramUtils.h
+++ src/Solver/Cpp/analysis/NGramUtils.h
@@ -4,6 +4,7 @@
 template<typename T,int N>
 std::vector<std::array<T, N>> getNGram(std::vector<T> word) {
 	std::vector<std::array<T, N>> returnable;
+	returnable.reserve(word.size());
 	for (int i = 0; i <= word.size() - N; i++) {
 		std::array<T, N> nGram;
 		for (int j = 0; j < N; j++) {
--- src/Solver/Cpp/ngramdatabuilder.cpp
+++ src/Solver/Cpp/ngramdatabuilder.cpp
@@ -4,9 +4,10 @@
 #include <map>
 #include <array>
 #include <string>
+#include <unordered_map>
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
-#include <boost/serialization/map.hpp>
+#include <boost/serialization/unordered_map.hpp>
 #include <boost/serialization/array.hpp>
 
 int main()
@@ -14,8 +15,47 @@ int main()
     std::string filename;
     std::cout << "Filename: ";
     std::cin >> filename;
+    std::string outname;
+    std::cout << "Output fileprefix: ";
+    std::cin >> outname;
     std::string datain;
-    std::ifstream fin(filename);
+    {
+        std::ifstream fin(filename);
+        std::getline(fin, datain);
+    }
+    std::vector<int> data;
+    data.reserve(datain.size());
+    for (char c : datain) {
+        data.push_back((int)c - 64);
+    }
+    std::vector<std::array<int, 2>> bigrams = getNGram<int,2>(data);
+    std::vector<std::array<int, 3>> trigrams = getNGram<int, 3>(data);
+    std::vector<std::array<int, 4>> quadgrams = getNGram<int, 4>(data);
+
+    std::unordered_map<std::array<int, 2>,int> bigramFrequencies;
+    std::unordered_map<std::array<int, 3>, int> trigramFrequencies;
+    std::unordered_map<std::array<int, 4>, int> quadgramFrequencies;
+
+    for (std::array<int, 2> key : bigrams) bigramFrequencies[key]++;
+    for (std::array<int, 3> key : trigrams) trigramFrequencies[key]++;
+    for (std::array<int, 4> key : quadgrams) quadgramFrequencies[key]++;
+    {
+        std::ofstream biout(outname + "bi.bin");
+        boost::archive::binary_oarchive oa(biout);
+        boost::serialization::save(oa, bigramFrequencies, 1);
+    }
+    {
+        std::ofstream triout(outname + "tri.bin");
+        boost::archive::binary_oarchive oa(triout);
+        boost::serialization::save(oa, trigramFrequencies, 1);
+    }
+    {
+        std::ofstream quadout(outname + "quad.bin");
+        boost::archive::binary_oarchive oa(quadout);
+        boost::serialization::save(oa, quadgramFrequencies, 1);
+    }
+
+
     /*std::map<std::array<int, 3>, int> myMap = {{{1,2,3}, 4}, {{4,5,6}, 7}, {{7,8,9}, 10}};
 
     std::cout << "checkpoint1\n";
