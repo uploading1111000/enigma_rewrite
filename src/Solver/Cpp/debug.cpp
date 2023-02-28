2dacf42849dc03d0c790e4b1aa6f634d417766b0
uploading1111000 <alexkdaniels@icloud.com>
2023-02-28 12:35:38 +0000

    solver start

--- src/Solver/Cpp/debug.cpp
+++ src/Solver/Cpp/debug.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
-#include "indexOfCoincidence.h"
-#include "utils.h"
-
-std::string clean(std::string in) {
-	std::string returnable;
-	for (int letter : in) {
-		if (letter > 64 and letter < 91) returnable.push_back((char)letter);
-		else if (letter > 96 and letter < 123) returnable.push_back((char)letter - 32);
-	}
-	return returnable;
-}
-
-const int N = 3; // size of the array
+#include "solverMachine.h"
 
 int main(){
 
