adca3baedd03a99a57db66b51ba7cff3f4f723f4
uploading1111000 <alexkdaniels@icloud.com>
2023-03-12 13:36:16 +0000

    refactor

--- src/Solver/solve.py
+++
@@ -1,9 +0,0 @@
-import importlib
-import os
-binds = importlib.import_module("Cpp.out.build.x64-Release.binds")
-specifications = []
-specifications.append(binds.MachineSpecification("src/Solver/Cpp/simulation/machineJsons/enigmaI.json"))
-specifications.append(binds.MachineSpecification("src/Solver/Cpp/simulation/machineJsons/enigmaM3.json"))
-specifications.append(binds.MachineSpecificationFour("src/Solver/Cpp/simulation/machineJsons/enigmaM4.json"))
-machine4 = binds.MachineFour(specifications[2])
-print(machine4.encryptWord("HELLO"))
